94b4a50ef917918bae3616de48169c9bfc9cdfe9
MohitWayde <62068704+MohitWayde@users.noreply.github.com>
2021-04-03 16:28:22 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls.conf import include
 from accounts import views
 
 urlpatterns = [
-    path('index', views.index,name='index'),
+    path('', views.index,name='index'),
     path('signin', views.signin,name='signin'),
     path('signup', views.signup,name='signup'),
     path('dashboard', views.dashboard,name='dashboard'),
