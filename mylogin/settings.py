325fc163a9b819a42285d9a80a73ecfb2f702507
MohitWayde <62068704+MohitWayde@users.noreply.github.com>
2021-04-03 14:54:36 +0530

    Update settings.py

--- mylogin/settings.py
+++ mylogin/settings.py
@@ -118,14 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-    BASE_DIR / "static",
+
+STATICFILES_DIRS = [
     '/mylogin/static/',
-    
 ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # SESSION_COOKIE_AGE = 20
